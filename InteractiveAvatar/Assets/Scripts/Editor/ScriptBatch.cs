ce36db6bfc672b13f6402d60e3f0335fa941dfcb
Joshua <joshcodedev@gmail.com>
2018-05-16 15:17:18 +0200

    Add button in editor to experiment with buildscripts
    We need a custom buildscript in order to be able to compile other open source projects that we will use.
    Jesus Christ Unity. Write better documentation and tools for this

---
+++ InteractiveAvatar/Assets/Scripts/Editor/ScriptBatch.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using UnityEditor;
+
+public class ScriptBatch
+{
+    [MenuItem("MyTools/Windows Build With Postprocess")]
+    public static void BuildGame()
+    {
+        UnityEngine.Debug.Log("Building haha");
+        System.Diagnostics.Debug.WriteLine("Building");
+        // Get filename.
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Site", "", "");
+        UnityEngine.Debug.Log(path);
+//        string[] scenes = new string[] {"Assets/_Scenes/InteractiveAvatar.unity"};
+
+        // Build player.
+//        BuildPipeline.BuildPlayer(scenes, path, BuildTarget.WebGL, BuildOptions.None);
+    }
+}
