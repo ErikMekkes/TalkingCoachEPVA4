f5d8d79ccac72b1414962a3953e7947cada5a285
Lucile Nikkels <luus.nikkels@gmail.com>
2018-05-23 16:11:04 +0200

    Add first tests. Failing because of issues with DLLImport.

---
+++ InterActiveAvatar/Assets/Tests/Editor/TextManagerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Runtime.InteropServices;
+
+public class TextManagerTest {
+
+    //TextManager tm = GameObject.FindObjectOfType<TextManager>();
+    private TextManager tm;
+
+	[Test]
+    public void StartSpeechTest()
+    {
+        tm = new TextManager();
+        Assert.IsFalse(tm.getIsSpeaking());
+        tm.startSpeach("This is a string that is used for testing the start speech function");
+        Assert.IsTrue(tm.getIsSpeaking());
+    }
+
+}
