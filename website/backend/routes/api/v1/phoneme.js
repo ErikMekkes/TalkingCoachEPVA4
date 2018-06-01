cec5e7d42c4f572e6c66488aef624a9940989e5a
JoshCode <joshcodedev@gmail.com>
2018-06-01 06:21:46 +0200

    Add API structure and server side phoneme translation, not yet ARPA

---
+++ website/backend/routes/api/v1/phoneme.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const child_process = require('child_process');
+const {spawn} = require('child_process');
+
+/* GET /api/v1/phoneme */
+router.get('/', function(req, res, next) {
+	let params = req.query;
+
+	/* Test if text query is defined */
+	if(params.text === undefined) {
+		res.status(404).json({ _request: {route: req.baseUrl, query: req.query, api_ver: "v1", error: "malformed query - text not defined"} });
+		return;
+	}
+	if(params.lang === undefined) {
+		res.status(404).json({ _request: {route: req.baseUrl, query: req.query, api_ver: "v1", error: "malformed query - lang not defined"} });
+		return;
+	}
+
+	// const espeak = spawn('espeak-ng', ['-qx', '-v', `${params.lang}`, '--sep=.', '"' + params.text.trim() + '"']);
+	const espeak = child_process.exec(`espeak-ng -qx -v ${params.lang} --sep=. "${params.text}"`);
+	let stdout = "";
+
+	espeak.stdout.on('data', (data) => {
+		// console.log("stdout: " + data);
+		stdout += data;
+	});
+	espeak.on('close', (code) => {
+		let phonemeString = cleanPhonemeString(stdout);
+		console.log(phonemeString);
+		let phonemeArray = getPhonemeArrayFromString(phonemeString);
+		let phonemeArrayArpa = phonemeArrayToArpabet(phonemeArray);
+		res.status(200).json({ _request: {route: req.baseUrl, query: req.query, api_ver: "v1"}, phonemes: phonemeArrayArpa });
+	});
+});
+
+function cleanPhonemeString(messyString) {
+	let result = messyString;
+	result = result.trim();
+	result = result.replace(/[_:!',|]/gi, "");
+	result = result.trim();
+	return result;
+}
+
+function getPhonemeArrayFromString(phonemeString) {
+	return phonemeString.split(".");
+}
+
+function phonemeArrayToArpabet(phonemeArray) {
+	let result = [];
+	for(let i = 0; i < phonemeArray.length; i++) {
+		let phoneme = phonemeArray[i];
+		let phonemeArpa = "";
+		switch(phoneme) {
+			default:
+				phonemeArpa = 'invalid phoneme';
+				break;
+		}
+		result[i] = phonemeArpa;
+	}
+	return result;
+}
+
+module.exports = router;
