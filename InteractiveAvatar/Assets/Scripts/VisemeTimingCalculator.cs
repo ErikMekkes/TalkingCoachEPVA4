2c337efa7cac2d490c02ccf9f85c618a882a4ebf
Muhammed Imran Özyar <m.ozyar@outlook.com>
2018-06-01 09:52:44 +0200

    Remove underscores and remove static variable.

--- InteractiveAvatar/Assets/Scripts/VisemeTimingCalculator.cs
+++ InteractiveAvatar/Assets/Scripts/VisemeTimingCalculator.cs
@@ -18,26 +18,26 @@ public class VisemeTimingCalculator
     /// <summary>
     /// The in-memory map of viseme to double (duration).
     /// </summary>
-    private static IDictionary<string, double> _visemeDictionary;
+    private IDictionary<string, double> visemeDictionary;
     
     /// <summary>
     /// The Singleton instance of the class.
     /// </summary>
-    private static VisemeTimingCalculator _instance;
+    private static VisemeTimingCalculator instance;
     
     /// <summary>
     /// The initiation of the singleton: either returns the instance of it already exists and creates an instantiates
     /// an instance otherwise.
     /// </summary>
-    public static VisemeTimingCalculator instance
+    public static VisemeTimingCalculator getInstance
     {
         get
         {
-            if (_instance == null)
+            if (instance == null)
             {
-                _instance = new VisemeTimingCalculator();
+                instance = new VisemeTimingCalculator();
             }
-            return _instance;
+            return instance;
         }
     }
 
@@ -46,19 +46,19 @@ public class VisemeTimingCalculator
     /// </summary>
     public void constructDictionary(StreamReader reader)
     {
-        if (_visemeDictionary != null)
+        if (visemeDictionary != null)
         {
             return;
         }
 
-        _visemeDictionary = new Dictionary<string, double>();
+        visemeDictionary = new Dictionary<string, double>();
         
         string line;
         while ((line = reader.ReadLine()) != null)
         {
             var splitLine = line.Split(',');
             Debug.Log(splitLine[0] + " : " + splitLine[1]);
-            _visemeDictionary[splitLine[0]] = float.Parse(splitLine[1]);
+            visemeDictionary[splitLine[0]] = float.Parse(splitLine[1]);
         }
         
         reader.Close();
@@ -70,7 +70,7 @@ public class VisemeTimingCalculator
     /// <returns>The dictionary of visemes mapped to their corresponding lengths.</returns>
     public IDictionary<string, double> getDictionary()
     {
-        return _visemeDictionary;
+        return visemeDictionary;
     } 
 
     /// <summary>
@@ -80,7 +80,7 @@ public class VisemeTimingCalculator
     /// <returns>A list of doubles, starting with 0, ascendingly ordered.</returns>
     public List<double> getVisemeDurations(IEnumerable<string> visemes)
     {
-        if (_visemeDictionary == null)
+        if (visemeDictionary == null)
         {
             constructDictionary(new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(Language.text))));
             return getVisemeDurations(visemes);
@@ -90,7 +90,7 @@ public class VisemeTimingCalculator
         foreach (var t in visemes)
         {
             durationList.Add(currentDuration);
-            currentDuration += _visemeDictionary[t];
+            currentDuration += visemeDictionary[t];
         }
 
         return durationList;
--- InteractiveAvatar/Assets/Tests/Editor/VisemeTestingCalculatorTest.cs
+++ InteractiveAvatar/Assets/Tests/Editor/VisemeTestingCalculatorTest.cs
@@ -7,7 +7,7 @@ public class VisemeTestingCalculatorTest
     [Test]
     public void readDictionaryTest()
     {
-        VisemeTimingCalculator vtc = VisemeTimingCalculator.instance;
+        VisemeTimingCalculator vtc = VisemeTimingCalculator.getInstance;
         vtc.constructDictionary(new StreamReader("Assets/Resources/Languages/eng-viseme.txt"));
         Assert.IsTrue(vtc.getDictionary()["AE"] > 0.260d && vtc.getDictionary()["AE"] < 0.280d );
     }
@@ -15,7 +15,7 @@ public class VisemeTestingCalculatorTest
     [Test]
     public void getVisemeDurationsTest()
     {
-        VisemeTimingCalculator vtc = VisemeTimingCalculator.instance;
+        VisemeTimingCalculator vtc = VisemeTimingCalculator.getInstance;
         vtc.constructDictionary(new StreamReader("Assets/Resources/Languages/eng-viseme.txt"));
         var visemeList = vtc.getVisemeDurations(new List<string>() {"AE", "AO", "AY"});
         Assert.IsTrue(visemeList[0] == 0 && visemeList[1] > 0 && visemeList[2] > visemeList[1]);
