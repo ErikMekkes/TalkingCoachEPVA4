c571b70b65844217a24451ad4c64a4c80df4b397
Muhammed Imran Özyar <m.ozyar@outlook.com>
2018-06-01 10:14:03 +0200

    Add initial body for combined algorithms.

---
+++ InteractiveAvatar/Assets/Scripts/LipSynchronization.cs
@@ -0,0 +1,28 @@
+﻿public class LipSynchronization
+{
+    private static LipSynchronization instance;
+    
+    private LipSynchronization() {}
+    
+    /// <summary>
+    /// The initiation of the singleton: either returns the instance of it already exists and creates an instantiates
+    /// an instance otherwise.
+    /// </summary>
+    public static LipSynchronization getInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new LipSynchronization();
+            }
+            return instance;
+        }
+    }
+
+    public void synchronize(string text)
+    {
+        // Text to phonemes and possible phonemes to visemes call
+        
+    }
+}
