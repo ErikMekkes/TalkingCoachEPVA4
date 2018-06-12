b9df2bad9736ec29f13bc493f298d10cf60f4864
Muhammed Imran Özyar <m.ozyar@outlook.com>
2018-06-12 20:04:20 +0200

    Add unit tests for Visemes.

--- InteractiveAvatar/Assets/Tests/Editor/Models/VisemeTest.cs
+++ InteractiveAvatar/Assets/Tests/Editor/Models/VisemeTest.cs
@@ -1,7 +1,22 @@
-﻿namespace Tests.Editor
+﻿using Models;
+using NUnit.Framework;
+
+namespace Tests.Editor
 {
     public class VisemeTest
     {
-        
+        public void getVisemeCodeTest()
+        {
+            var phoneme = "AA";
+            var viseme = Phoneme.getPhonemeFromCode(phoneme).toViseme();
+            Assert.AreEqual("AA", viseme.getVisemeCode());
+        }
+
+        public void getDurationTest()
+        {
+            var phoneme = "AA";
+            var viseme = Phoneme.getPhonemeFromCode(phoneme).toViseme();
+            Assert.IsTrue(viseme.getDuration() > 0.300 && viseme.getDuration() < 0.320);
+        }
     }
 }
