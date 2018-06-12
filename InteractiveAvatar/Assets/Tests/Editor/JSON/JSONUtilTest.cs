63ade12065307ef8e35b3d1acd44be6a56ffb0b6
Muhammed Imran Özyar <m.ozyar@outlook.com>
2018-06-12 20:13:00 +0200

    Add JSONUtil test and fix Viseme model testing.

---
+++ InteractiveAvatar/Assets/Tests/Editor/JSON/JSONUtilTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using SimpleJSON;
+
+namespace Tests.Editor.JSON
+{
+    public class JSONUtilTest
+    {
+        [Test]
+        public void arrayToListTest()
+        {
+            var jsonArray = new JSONArray();
+            jsonArray.Add(0);
+            jsonArray.Add(1);
+            jsonArray.Add(2);
+
+            var jsonList = JSONUtil.arrayToList(jsonArray);
+            
+            Assert.AreEqual(jsonArray[0].ToString(), jsonList[0]);
+            Assert.AreEqual(jsonArray[1].ToString(), jsonList[1]);
+            Assert.AreEqual(jsonArray[2].ToString(), jsonList[2]);
+        }
+    }
+}
