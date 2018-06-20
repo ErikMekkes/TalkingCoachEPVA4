6d8c3c3cf1fd69d6862ff2ce5b7cf3c04c3b19e6
Muhammed Imran Özyar <m.ozyar@outlook.com>
2018-06-20 15:45:32 +0200

    Add code coverage tool along with extra tests.

--- website/backend/test/phonemeTest.js
+++ website/backend/test/phonemeTest.js
@@ -3,8 +3,8 @@ const httpMocks = require("node-mocks-http");
 const EventEmitter =  require('events');
 const exampleRouteHandler = require("../routes/api/v1/phoneme");
 
-  describe("Example Router", () => {
-    it("should return 'hello world' for GET /example", function (done) {
+  describe("Test text to phoneme conversion.", () => {
+    it("Hello world", function (done) {
       const mockRequest = httpMocks.createRequest({
         method: "GET",
         url: "/?text=Hello&lang=en-US",
@@ -19,9 +19,48 @@ const exampleRouteHandler = require("../routes/api/v1/phoneme");
       const expectedResponseBody = [ 'HH', 'AH', 'L', 'OW' ];
 
       mockResponse.on('send', () => {
-        console.log("data = " +  JSON.parse(mockResponse._getData()).phonemes);
-        assert.deepEqual(JSON.parse(mockResponse._getData()).phonemes,expectedResponseBody);
+        assert.deepEqual(JSON.parse(mockResponse._getData()).phonemes, expectedResponseBody);
         done();
       });
     });
+
+    it("Introduction sentence", function (done) {
+        const mockRequest = httpMocks.createRequest({
+          method: "GET",
+          url: "/?text=Hello we are epva4&lang=en-US",
+          params: {
+            text: "Hello",
+            lang: 'en-US'
+          }
+        });
+        const mockResponse = httpMocks.createResponse({eventEmitter: EventEmitter});
+        exampleRouteHandler(mockRequest, mockResponse);
+        
+        const expectedResponseBody = [ 'HH', 'AH', 'L', 'OW', '/', 'W', 'IY', '/', 'AA', 'R', 'R', '/', 'EH', 'P', 'V', 'AH', '/', 'F', 'AO' ];
+  
+        mockResponse.on('send', () => {
+          assert.deepEqual(JSON.parse(mockResponse._getData()).phonemes, expectedResponseBody);
+          done();
+        });
+      });
+
+      it("Demo sentence", function (done) {
+        const mockRequest = httpMocks.createRequest({
+          method: "GET",
+          url: "/?text=The quick brown fox jumps over the lazy dog&lang=en-US",
+          params: {
+            text: "Hello",
+            lang: 'en-US'
+          }
+        });
+        const mockResponse = httpMocks.createResponse({eventEmitter: EventEmitter});
+        exampleRouteHandler(mockRequest, mockResponse);
+        
+        const expectedResponseBody = [ 'DH', 'AH', '/', 'K', 'W', 'IH', 'K', '/', 'B', 'R', 'AW', 'N', '/', 'F', 'AA', 'K', 'S', '/', 'JH', 'AH', 'M', 'P', 'S', '/', 'OW', 'V', 'ER', '/', 'DH', 'AH', '/', 'L', 'EY', 'Z', 'IY', '/', 'D', 'AA', 'G' ];
+  
+        mockResponse.on('send', () => {
+          assert.deepEqual(JSON.parse(mockResponse._getData()).phonemes, expectedResponseBody);
+          done();
+        });
+      });
   });
