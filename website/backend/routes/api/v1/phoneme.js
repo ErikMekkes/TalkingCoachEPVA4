1daa523909e208dc92d47baefe5abb5419bf4f94
Joshua Slik <joshcodedev@gmail.com>
2018-06-22 16:48:30 +0200

    Document phoneme functions in backend API

--- website/backend/routes/api/v1/phoneme.js
+++ website/backend/routes/api/v1/phoneme.js
@@ -8,7 +8,10 @@ const dict = new PhonemeDictionary();
 
 let warnings = [];
 
-/* GET /api/v1/phoneme */
+/**
+ * Create point for the router to resolve the following HTTP request
+ * GET /api/v1/phoneme
+ */
 router.get('/', function (req, res, next) {
 	let params = req.query;
 	warnings = [];
@@ -17,9 +20,9 @@ router.get('/', function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-	/* Test if text query is defined */
+	/* Test if text parameter is defined, return 400 error if not defined */
 	if (params.text === undefined) {
-		res.status(404).json({
+		res.status(400).json({
 			_request: {
 				route: req.baseUrl,
 				query: req.query,
@@ -29,8 +32,9 @@ router.get('/', function (req, res, next) {
 		});
 		return;
 	}
+	/* Test if lang parameter is defined, return 400 error if not defined */
 	if (params.lang === undefined) {
-		res.status(404).json({
+		res.status(400).json({
 			_request: {
 				route: req.baseUrl,
 				query: req.query,
@@ -46,9 +50,11 @@ router.get('/', function (req, res, next) {
 	let espeak = child_process.exec(`espeak-ng -qx -v ${params.lang} --sep=. "${params.text}"`);
 	let stdout = "";
 
+	// Read stdout of eSpeak command and store it on stdout variable
 	espeak.stdout.on('data', (data) => {
 		stdout += data;
 	});
+	// Handle translation of eSpeak output once command finishes
 	espeak.on('close', (code) => {
 		console.log(code);
 		let phonemeString = cleanPhonemeString(stdout, params.lang);
@@ -68,6 +74,15 @@ router.get('/', function (req, res, next) {
 	});
 });
 
+/**
+ * This function cleans a string of unused characters. Cleanup may vary based on selected language.
+ * Future developers should extend this function if they intend to use other languages
+ * Throws a soft warning if language is unknown
+ *
+ * @param messyString The string to trim and clean of unnecessary characters
+ * @param language The selected language, cleanup varies per language
+ * @returns {string} Cleaned up string
+ */
 function cleanPhonemeString(messyString, language) {
 	let result = messyString;
 	console.log(messyString);
@@ -85,11 +100,22 @@ function cleanPhonemeString(messyString, language) {
 	return result;
 }
 
+/**
+ * Splits a string of cleaned up phonemes into an array of individual phonemes
+ *
+ * @param phonemeString String of cleaned up phonemes
+ * @returns {*|string[]} Array of individual phonemes as strings in eSpeak notation
+ */
 function getPhonemeArrayFromString(phonemeString) {
 	phonemeString = phonemeString.replace(/[ ]/gi, './.');
 	return phonemeString.split(/[. ]/gi);
 }
 
+/**
+ * Translate eSpeak phoneme notation to ARPABET phoneme notation based on mapping specified in phonemeDictionary.json file
+ * @param phonemeArray Array of individual phonemes as strings in eSpeak notation
+ * @returns {Array} Array of individual phonemes as strings in ARPABET notation
+ */
 function phonemeArrayToArpabet(phonemeArray) {
 	let result = [];
 	console.log(phonemeArray);
@@ -103,7 +129,6 @@ function phonemeArrayToArpabet(phonemeArray) {
 			continue;
 		}
 
-		// TODO: Can be better, perhaps with a Map
 		if (dict.dictionary.has(phoneme)) {
 			dict.dictionary.get(phoneme).forEach((value) => {
 				result.push(value);
