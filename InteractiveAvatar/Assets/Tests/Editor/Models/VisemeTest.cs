2aa0a2bf5181f62c1e11575bf4a555ae517d9b94
Erik Mekkes <E.J.Mekkes@gmail.com>
2018-06-22 14:28:32 +0200

    Add some basic unit testing for SpeechAnimationManager

--- InteractiveAvatar/Assets/Tests/Editor/Models/VisemeTest.cs
+++ InteractiveAvatar/Assets/Tests/Editor/Models/VisemeTest.cs
@@ -20,11 +20,11 @@ namespace Tests.Editor
             var phoneme = "AA";
             var viseme = Phoneme.getPhonemeFromCode(phoneme).toViseme();
 
+            var expectedDuration = 0.275f;
             // VisemeTimings instance has been created here locally since the actual used one in the code is defined through the Unity UI.
             var vtc = VisemeTimings.getInstance;
             vtc.constructDictionary(new StreamReader("Assets/Resources/Languages/eng-viseme.txt"));
-            Assert.IsTrue(vtc.getDictionary()[viseme.getVisemeCode().getName()] > 0.300 &&
-                          vtc.getDictionary()[viseme.getVisemeCode().getName()] < 0.320);
+            Assert.AreEqual(expectedDuration, vtc.getDictionary()[viseme.getVisemeCode().getName()]);
         }
     }
 }
